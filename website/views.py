7e9dc500a72abaa1b02675b4a449cd9fcccbd7a3
owen222 <64684395+owen222@users.noreply.github.com>
2021-07-22 19:24:53 +0800

    Initial Commit

---
+++ website/views.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template, request, flash, jsonify, make_response
+from . import db
+import json
+
+views = Blueprint('views', __name__)
+
+
+@views.route('/')
+def home():
+    return render_template("home.html")
+
+
+@views.route('/typing-patterns')
+def typing_patterns():
+    return render_template("typing_patterns.html")
